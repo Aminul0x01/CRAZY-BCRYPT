bc85b9130319f7f4cb1f977e1c4ecd7ce66d283d
CRAZY CODDER <71552351+crazy-root@users.noreply.github.com>
2022-09-28 13:11:08 +0530

    Create crazy-bcrypt.py

---
+++ crazy-bcrypt.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# bcrypt decode single
+# Open Source Dedoce gagaltotal666 - Tot
+
+from passlib.hash import bcrypt
+from libsrary import pingo
+import os
+import sys
+
+print ("\n")
+print ("===============================   Welcome To Crazy-Root     ===============================")
+print ("= ██████╗██████╗  █████╗ ███████╗██╗   ██╗     ██████╗  ██████╗██████╗ ██╗   ██╗██████╗ ████████╗=")
+print ("=██╔════╝██╔══██╗██╔══██╗╚══███╔╝╚██╗ ██╔╝     ██╔══██╗██╔════╝██╔══██╗╚██╗ ██╔╝██╔══██╗╚══██╔══╝=")
+print ("=██║     ██████╔╝███████║  ███╔╝  ╚████╔╝█████╗██████╔╝██║     ██████╔╝ ╚████╔╝ ██████╔╝   ██║   =")
+print ("=██║     ██╔══██╗██╔══██║ ███╔╝    ╚██╔╝ ╚════╝██╔══██╗██║     ██╔══██╗  ╚██╔╝  ██╔═══╝    ██║   =")
+print ("=╚██████╗██║  ██║██║  ██║███████╗   ██║        ██████╔╝╚██████╗██║  ██║   ██║   ██║        ██║   =")
+print ("= ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝   ╚═╝        ╚═════╝  ╚═════╝╚═╝  ╚═╝   ╚═╝   ╚═╝        ╚═╝   =")
+print ("============================== https://github.com/crazy-root ==============================")
+print ("\n")
+
+options = input('decode bcrypt get bro ? type (y) to continue decoding, key (ctrl+c) not continue decoding : ')
+
+if (options != "y" and options != "n"):
+    sys.exit('Invalid Option')
+
+passwords = (options == "y")
+text_file = open("wordlist.txt", "r")
+
+words = text_file.read().splitlines()
+
+hash = input('enter bcrypt hash : ')
+length = len(words)
+
+correct_word = ""
+for (index, word) in enumerate(words):
+    pingo(index, length, prefix='Wait for a while :', suffix='Complete')
+    correct = bcrypt.verify(word, hash)
+    if (correct):
+        correct_word = word
+        print()
+        break
+
+print("Decode Bcrypt result :", correct_word)
+
